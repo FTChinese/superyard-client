fb6ac3150b8d18f8729fc4d8a18fa25606ad680c
neefrankie <neefrankie@gmail.com>
2020-02-10 14:09:12 +0800

    refactor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule} from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccountService } from "./core/account.service";
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { LoginComponent } from './login/login.component';
+import { FlashComponent } from './flash/flash.component';
+import { FeedbackInvalidComponent } from './feedback-invalid/feedback-invalid.component';
+import { FlashDirective } from './core/flash.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    LoginComponent,
+    FlashComponent,
+    FeedbackInvalidComponent,
+    FlashDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,ReactiveFormsModule,
+    RouterModule.forRoot([
+      { path: "", component: LoginComponent },
+    ]),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [AccountService],
+  bootstrap: [AppComponent],
+  entryComponents: [
+    FlashComponent,
+  ]
 })
 export class AppModule { }
