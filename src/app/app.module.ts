39581ab9abf93b57e0ed4c1203db6307a0b484fb
neefrankie <neefrankie@gmail.com>
2020-02-12 06:34:28 +0800

    refactor: split into features

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
 import { HttpClientModule} from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 
@@ -8,27 +7,25 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AccountService } from "./core/account.service";
 import { TopBarComponent } from './top-bar/top-bar.component';
-import { LoginComponent } from './login/login.component';
 import { FlashComponent } from './flash/flash.component';
 import { FeedbackInvalidComponent } from './feedback-invalid/feedback-invalid.component';
 import { FlashDirective } from './core/flash.directive';
+import { LoginModule } from './login/login.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopBarComponent,
-    LoginComponent,
     FlashComponent,
     FeedbackInvalidComponent,
     FlashDirective
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    LoginModule,
     AppRoutingModule,
-    HttpClientModule,ReactiveFormsModule,
-    RouterModule.forRoot([
-      { path: "", component: LoginComponent },
-    ]),
   ],
   providers: [AccountService],
   bootstrap: [AppComponent],
--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -124,7 +124,6 @@ export class LoginComponent implements OnInit {
   }
 
   clearFeedback() {
-    console.log('input clicked');
     this.nameInvalid = '';
     this.pwInvalid = '';
   }
