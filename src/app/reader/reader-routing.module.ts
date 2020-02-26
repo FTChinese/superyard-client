ae38c79131c61b518512d8b3ebf19df5b286f71e
weiguo.ni <neefrankie@gmail.com>
2020-02-26 17:57:44 +0800

    reafactor: remove unused routes

--- src/app/reader/reader-routing.module.ts
+++ src/app/reader/reader-routing.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AccountDetailComponent } from './account-detail/account-detail.component';
 import { ReaderHomeComponent } from './reader-home/reader-home.component';
-import { MembershipComponent } from './membership/membership.component';
-import { EditMembershipComponent } from './edit-membership/edit-membership.component';
 import { ProfileComponent } from './profile/profile.component';
 
 
@@ -17,10 +15,6 @@ const routes: Routes = [
     component: AccountDetailComponent,
     data: { kind: 'ftc' },
     children: [
-      {
-        path: 'create-subscription',
-        component: MembershipComponent,
-      },
       {
         path: 'profile',
         component: ProfileComponent,
