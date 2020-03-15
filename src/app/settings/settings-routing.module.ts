7dd503518d2d9a80a49c18db5292edab1d0c9db9
weiguo.ni <neefrankie@gmail.com>
2020-03-15 14:41:05 +0800

    feat: settings router

--- src/app/settings/settings-routing.module.ts
+++ src/app/settings/settings-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProfileComponent } from './profile/profile.component';
+import { PasswordComponent } from './password/password.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'profile',
+    component: ProfileComponent,
+  },
+  {
+    path: 'password',
+    component: PasswordComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
