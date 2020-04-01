89391ab3de73aad916a6594799b68b0b9b5f0229
weiguo.ni <neefrankie@gmail.com>
2020-04-01 15:15:14 +0800

    use dynamic form

--- src/app/forgot-password/forgot-password/forgot-password.component.ts
+++ src/app/forgot-password/forgot-password/forgot-password.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { FormService } from 'src/app/shared/form.service';
+import { Validators } from '@angular/forms';
+import { InputControl, DynamicControl } from 'src/app/shared/control';
+import { Button } from 'src/app/shared/button';
 
 @Component({
   selector: 'app-forgot-password',
   templateUrl: './forgot-password.component.html',
-  styleUrls: ['./forgot-password.component.scss']
+  styleUrls: ['./forgot-password.component.scss'],
+  providers: [FormService],
 })
 export class ForgotPasswordComponent implements OnInit {
 
-  constructor() { }
+  controlConfigs: DynamicControl[] = [
+    new InputControl({
+      value: '',
+      key: 'name',
+      validators: [Validators.required, Validators.email, Validators.maxLength(64)],
+      label: 'Email',
+      placeholder: 'example@ftchinese.com',
+      type: 'email',
+    }),
+  ];
+
+  button: Button = {
+    block: true,
+    text: 'Send me a reset letter'
+  };
+
+  constructor(
+    private formService: FormService,
+  ) { }
 
   ngOnInit(): void {
+    this.formService.formSubmitted$.subscribe(data => {
+      console.log(data);
+    });
   }
 
 }
