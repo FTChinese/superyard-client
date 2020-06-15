0114d7bae107b1c2bb0776cc423d5606ddc9ca63
weiguo.ni <neefrankie@gmail.com>
2020-06-15 19:14:04 +0800

    refactor: show message after email sent

--- src/app/modules/auth/forgot-password/forgot-password.component.ts
+++ src/app/modules/auth/forgot-password/forgot-password.component.ts
@@ -3,6 +3,9 @@ import { FormService } from 'src/app/shared/service/form.service';
 import { Validators } from '@angular/forms';
 import { InputControl, DynamicControl } from 'src/app/shared/widget/control';
 import { Button } from 'src/app/shared/widget/button';
+import { EmailForm } from 'src/app/data/schema/form-data';
+import { authUrls } from 'src/app/layout/sitemap';
+import { Alert } from 'src/app/shared/widget/alert';
 
 @Component({
   selector: 'app-forgot-password',
@@ -28,14 +31,30 @@ export class ForgotPasswordComponent implements OnInit {
     .setBlock()
     .setName('Send me a reset letter');
 
+  loginLink = authUrls.login;
+
+  alert: Alert;
+
+  private set alertMsg(v: string) {
+    this.alert = {
+      type: 'danger',
+      message: v,
+      dismissible: true,
+    };
+  }
+
   constructor(
     private formService: FormService,
   ) { }
 
   ngOnInit(): void {
     this.formService.formSubmitted$.subscribe(data => {
-      console.log(data);
+      const formData: EmailForm = JSON.parse(data);
+      console.log(formData);
     });
   }
 
+  onDismissAlert() {
+    this.alert = null;
+  }
 }
