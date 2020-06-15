a49100fb6347a7a37f93bd783dba16b252e49b99
weiguo.ni <neefrankie@gmail.com>
2020-06-15 22:00:53 +0800

    feat: add ResetPasswordComponent

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -5,11 +5,13 @@ import { SharedModule } from '../../shared/shared.module';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 import { AuthRoutingModule } from './auth-routing.module';
 import { CommonModule } from '@angular/common';
+import { ResetPasswordComponent } from './reset-password/reset-password.component';
 
 @NgModule({
   declarations: [
     LoginComponent,
     ForgotPasswordComponent,
+    ResetPasswordComponent,
   ],
   imports: [
     CommonModule,
