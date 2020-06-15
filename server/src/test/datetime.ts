dbd29f023c5648f4e8270ac29b0eb0b5b967ba15
weiguo.ni <neefrankie@gmail.com>
2020-06-15 21:03:40 +0800

    feat: removed mocking api server

--- server/src/test/datetime.ts
+++
@@ -1,5 +0,0 @@
-import { DateTime } from 'luxon';
-
-console.log(DateTime.fromISO('2016-05-25T09:08:34.123+06:00', {setZone: true}));
-
-console.log(DateTime.fromISO('2016-05-25T09:08:34Z', {setZone: true}));
--- server/src/time.ts
+++
@@ -1,5 +0,0 @@
-import { DateTime } from 'luxon';
-
-export function isoNow(): string {
-  return DateTime.utc().toISO({ suppressMilliseconds: true })
-}
