9e6395f8f66188e8a5ae14bbe7d019b3027be29a
weiguo.ni <neefrankie@gmail.com>
2020-06-16 11:45:59 +0800

    feat: create a search form

---
+++ src/app/shared/search-form/search-form.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { ControlOptions } from '../widget/control';
+import { Button } from '../widget/button';
+import { FormGroup, FormControl } from '@angular/forms';
+import { FormService } from '../service/form.service';
+
+@Component({
+  selector: 'app-search-form',
+  templateUrl: './search-form.component.html',
+  styleUrls: ['./search-form.component.scss'],
+  providers: [FormService],
+})
+export class SearchFormComponent implements OnInit {
+
+  @Input() control: ControlOptions;
+  @Input() button: Button;
+
+  form: FormGroup;
+
+  constructor(
+    private formService: FormService,
+  ) { }
+
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      [this.control.key]: new FormControl(this.control.value, this.control.validators)
+    });
+
+    this.formService.errorReceived$.subscribe(reqErr => {
+      this.form.enable();
+    })
+  }
+
+  onSubmit() {
+    this.formService.submit(JSON.stringify(this.form.getRawValue()));
+    this.form.disable();
+  }
+}
