7e756b7cb5d7fecfc5139f4d5ff3416163d985bf
weiguo.ni <neefrankie@gmail.com>
2020-06-16 11:46:56 +0800

    refactor: relocate

---
+++ src/app/modules/admin/components/staff-list/staff-list.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { IProfile } from 'src/app/data/schema/staff';
+
+@Component({
+  selector: 'app-staff-list',
+  templateUrl: './staff-list.component.html',
+  styleUrls: ['./staff-list.component.scss']
+})
+export class StaffListComponent implements OnInit {
+
+  profiles: IProfile[];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
