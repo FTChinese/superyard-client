2112e7c65578e604e0392ad5e9e03c26653f410e
weiguo.ni <neefrankie@gmail.com>
2020-06-16 15:16:22 +0800

    fix: import path

--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -4,11 +4,11 @@ import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminHomeComponent } from './page/admin-home/admin-home.component';
 import { StaffHomeComponent } from './page/staff-home/staff-home.component';
+import { CreateUserComponent } from './page/create-user/create-user.component';
+import { UpdateUserComponent } from './page/update-user/update-user.component';
 import { StaffListComponent } from './components/staff-list/staff-list.component';
 import { SearchResultsComponent } from './components/search-results/search-results.component';
-import { CreateUserComponent } from './create-user/create-user.component';
-import { UpdateUserComponent } from './update-user/update-user.component';
-import { UserFormComponent } from './user-form/user-form.component';
+import { UserFormComponent } from './components/user-form/user-form.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
