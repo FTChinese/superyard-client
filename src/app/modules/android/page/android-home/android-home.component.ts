29dc5a9c59f690f6ac9648e8b5b0cadcd5195c40
weiguo.ni <neefrankie@gmail.com>
2020-06-20 14:09:39 +0800

    refactor: cleanup

--- src/app/modules/android/page/android-home/android-home.component.ts
+++ src/app/modules/android/page/android-home/android-home.component.ts
@@ -1,12 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Validators } from '@angular/forms';
-import { ControlOptions } from 'src/app/shared/widget/control';
-import { Button } from 'src/app/shared/widget/button';
 import { FormService } from 'src/app/shared/service/form.service';
 import { IRelease } from 'src/app/data/schema/android';
-import { AndroidService } from 'src/app/data/service/android.service';
-import { switchMap } from 'rxjs/operators';
-import { SearchForm } from 'src/app/data/schema/form-data';
 
 @Component({
   selector: 'app-android-home',
