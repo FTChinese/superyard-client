647d64b0f497471aa49d1c2f4a2836578346053c
weiguo.ni <neefrankie@gmail.com>
2020-06-21 20:20:09 +0800

    feat: no card header under preview

--- src/app/modules/product/page/builder/builder.component.ts
+++ src/app/modules/product/page/builder/builder.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { BaseProduct, Plan } from 'src/app/data/schema/product';
+import { BaseProduct, Plan, Product } from 'src/app/data/schema/product';
+import { Cycle } from 'src/app/data/schema/enum';
 
-type ListAction = 'product' | 'plan';
 
 @Component({
   selector: 'app-builder',
@@ -12,38 +12,46 @@ export class BuilderComponent implements OnInit {
 
   product: BaseProduct;
 
-  yearlyPlan: Plan;
+  private plans: Map<Cycle, Plan> = new Map();
 
-  monthlyPlan: Plan;
-
-  action: ListAction = 'product';
-
-  get hasMonthPlan(): boolean {
-    if (!this.product) {
-      return false;
-    }
-
-    return this.product.tier === 'standard';
+  get yearlyPlan(): Plan | undefined {
+    return this.plans.get('year');
   }
 
-  get hasYearPlan(): boolean {
-    if (!this.product) {
-      return false;
-    }
+  get monthlyPlan(): Plan | undefined {
+    return this.plans.get('month');
+  };
 
-    return true;
+  get hasYearPlan(): boolean {
+    return !!this.product;
+  }
+
+  get hasMonthPlan(): boolean {
+    return this.product && this.product.tier === 'standard';
   }
 
   constructor() { }
 
   ngOnInit(): void {
+    const buildOn: Product | undefined = history.state.buildOn;
+    console.log(buildOn);
+
+    if (buildOn) {
+      this.product = {
+        id: buildOn.id,
+        tier: buildOn.tier,
+        heading: buildOn.heading,
+        description: buildOn.description,
+        smallPrint: buildOn.smallPrint,
+        createdUtc: buildOn.createdUtc,
+        createdBy: buildOn.createdBy,
+      };
+
+      buildOn.plans.forEach(plan => {
+        this.plans.set(plan.cycle, plan);
+      });
+    }
   }
 
-  onListProduct() {
-    this.action = 'product';
-  }
 
-  onListPlan() {
-    this.action = 'plan';
-  }
 }
