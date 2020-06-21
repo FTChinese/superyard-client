f036dca1be3adfd111852c207b5e3e2ec716e92d
weiguo.ni <neefrankie@gmail.com>
2020-06-21 20:18:42 +0800

    feat: added manual component

--- src/app/modules/product/product-routing.module.ts
+++ src/app/modules/product/product-routing.module.ts
@@ -4,6 +4,7 @@ import { ProductHomeComponent } from './page/product-home/product-home.component
 import { BuilderComponent } from './page/builder/builder.component';
 import { ProductListComponent } from './component/product-list/product-list.component';
 import { PlanListComponent } from './component/plan-list/plan-list.component';
+import { ManualComponent } from './component/manual/manual.component';
 
 const routes: Routes = [
   {
@@ -14,6 +15,10 @@ const routes: Routes = [
     path: 'builder',
     component: BuilderComponent,
     children: [
+      {
+        path: '',
+        component: ManualComponent,
+      },
       {
         path: 'products',
         component: ProductListComponent,
