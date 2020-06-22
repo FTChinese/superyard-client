e480e51bab5fe2d1c36e593a991c4d110b9442bb
weiguo.ni <neefrankie@gmail.com>
2020-06-22 21:39:53 +0800

    feat: builder service

--- src/app/core/service/product-builder.service.ts
+++ src/app/core/service/product-builder.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { Plan, BaseProduct } from 'src/app/data/schema/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductBuilderService {
 
+  private productSource = new Subject<BaseProduct>();
+  private planSource = new Subject<Plan>();
+
+  productSelected$ = this.productSource.asObservable();
+  planSelected$ = this.planSource.asObservable();
+
   constructor() { }
+
+  selectProduct(p: BaseProduct) {
+    this.productSource.next(p);
+  }
+
+  selectPlan(p: Plan) {
+    this.planSource.next(p);
+  }
 }
