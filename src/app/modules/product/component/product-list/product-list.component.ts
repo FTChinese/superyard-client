bd78b08583b28306099527cc1a670491dc401dd5
weiguo.ni <neefrankie@gmail.com>
2020-06-23 11:19:11 +0800

    feat: sync ui when a new product is created

--- src/app/modules/product/component/product-list/product-list.component.ts
+++ src/app/modules/product/component/product-list/product-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { BaseProduct } from 'src/app/data/schema/product';
 import { baseProdStd, baseProdPrm } from 'src/app/data/schema/mocker';
+import { ProductBuilderService } from 'src/app/core/service/product-builder.service';
 
 @Component({
   selector: 'app-product-list',
@@ -14,9 +15,15 @@ export class ProductListComponent implements OnInit {
     baseProdPrm,
   ];
 
-  constructor() { }
+  constructor(
+    private builderService: ProductBuilderService,
+  ) { }
 
   ngOnInit(): void {
+    this.builderService.productCreated$
+      .subscribe(prod => {
+        this.products.unshift(prod);
+      });
   }
 
 }
