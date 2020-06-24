05f894dca6de6ef6a438f7607174cacee3741294
weiguo.ni <neefrankie@gmail.com>
2020-06-24 09:48:53 +0800

    refactor: removed components

--- src/app/modules/product/product.module.ts
+++ src/app/modules/product/product.module.ts
@@ -5,8 +5,6 @@ import { ProductRoutingModule } from './product-routing.module';
 import { ProductHomeComponent } from './page/product-home/product-home.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { ProductContentComponent } from './component/product-content/product-content.component';
-import { B2bFormComponent } from './component/b2b-form/b2b-form.component';
-import { RetailFormComponent } from './component/retail-form/retail-form.component';
 import { BuilderComponent } from './page/builder/builder.component';
 import { PlanListComponent } from './component/plan-list/plan-list.component';
 import { ProductListComponent } from './component/product-list/product-list.component';
@@ -14,6 +12,7 @@ import { ProductItemComponent } from './component/product-item/product-item.comp
 import { PlanItemComponent } from './component/plan-item/plan-item.component';
 import { PlanContentComponent } from './component/plan-content/plan-content.component';
 import { ManualComponent } from './component/manual/manual.component';
+import { PlanFormComponent } from './component/plan-form/plan-form.component';
 
 
 @NgModule({
@@ -21,14 +20,13 @@ import { ManualComponent } from './component/manual/manual.component';
     ProductContentComponent,
     ProductHomeComponent,
     ProductListComponent,
-    B2bFormComponent,
-    RetailFormComponent,
     BuilderComponent,
     PlanListComponent,
     ProductItemComponent,
     PlanItemComponent,
     PlanContentComponent,
     ManualComponent,
+    PlanFormComponent,
   ],
   imports: [
     CommonModule,
