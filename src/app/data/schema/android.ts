b1035432b48d8278dc2e9a0168582057279633a1
weiguo.ni <neefrankie@gmail.com>
2020-07-17 16:44:08 +0800

    refactor: use AndroidRelease to replace other types

--- src/app/data/schema/android.ts
+++ src/app/data/schema/android.ts
@@ -1,42 +1,8 @@
-export interface IReleaseBase {
+export interface AndroidRelease {
   versionName: string;
   versionCode: number;
   body: string;
   apkUrl: string;
-}
-
-export interface IRelease extends IReleaseBase {
   createdAt: string;
   updatedAt: string;
-}
-
-export interface IGHContent {
-  name: string;
-  path: string;
-  content: string;
-  encoding: 'base64';
-}
-
-export interface IGradleFile {
-  apply: string[];
-  android: {
-      compileSdkVersion: string;
-      defaultConfig: {
-          applicationId: string;
-          minSdkVersion: string;
-          targetSdkVersion: string;
-          versionCode: string;
-          versionName: string;
-      }
-  };
-}
-
-export interface IGhRelease {
-  id: number;
-  tag_name: string;
-  name: string;
-  body: string;
-  draft: boolean;
-  created_at: string;
-  published_at: string;
 }
