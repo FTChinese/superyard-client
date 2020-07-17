e560d8be4837edc5edffd195425cad83839cb1c8
weiguo.ni <neefrankie@gmail.com>
2020-07-17 12:05:29 +0800

    refactor: differentiate error and information style

--- src/app/shared/service/toast.service.ts
+++ src/app/shared/service/toast.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { AlertType } from '../widget/alert';
 
 @Injectable({
   providedIn: 'root'
@@ -7,14 +8,31 @@ export class ToastService {
 
   message: string;
   timeoutID: number;
+  style: AlertType = 'primary';
+  get styleClassName(): string {
+    return `alert-${this.style}`;
+  }
 
   constructor() { }
 
   show(m: string) {
     this.message = m;
 
-    this.timeoutID = window.setTimeout(this.onDismiss.bind(this), 3000)
+    this.timeoutID = window.setTimeout(this.onDismiss.bind(this), 3000);
   }
+
+  info(m: string) {
+    this.style = 'primary';
+
+    this.show(m);
+  }
+
+  error(m: string) {
+    this.style = 'danger';
+
+    this.show(m);
+  }
+
 
   onDismiss() {
     this.message = null;
