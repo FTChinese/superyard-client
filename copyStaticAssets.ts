c2bfc4bac45dcbef960e1047bdd65dd77c6dc8d1
weiguo.ni <neefrankie@gmail.com>
2020-07-20 10:11:45 +0800

    refactor: copy target

--- copyStaticAssets.ts
+++ copyStaticAssets.ts
@@ -1,7 +1,11 @@
 import * as shell from 'shelljs';
 
 const htmlDir = '../superyard/web/views';
+const targetDir = '../ft-interact/superyard';
+
 shell.mkdir('-p', htmlDir);
-// shell.cp('dist/client/*.js', targetDir);
-// shell.cp('dist/client/*.css', targetDir);
+shell.mkdir('-p', targetDir);
+
+shell.cp('dist/client/*.js', targetDir);
+shell.cp('dist/client/*.css', targetDir);
 shell.cp('dist/home.html', htmlDir);
