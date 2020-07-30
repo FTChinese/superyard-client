097f4a447214c85cf1e979fb1d9968641ef1ea96
weiguo.ni <neefrankie@gmail.com>
2020-07-30 10:20:00 +0800

    feat: localizetion for payment method and order kind

--- src/app/data/schema/localization.ts
+++ src/app/data/schema/localization.ts
@@ -1,4 +1,4 @@
-import { Tier, Cycle } from './enum';
+import { Tier, Cycle, PaymentMethod, OrderKind } from './enum';
 
 export const tiers: Record <Tier, string> = {
   standard: '标准版',
@@ -10,3 +10,17 @@ export const cycles: Record<Cycle, string> = {
   month: '月',
   year: '年'
 };
+
+export const paymentMethods: Record<PaymentMethod, string> = {
+  alipay: '支付宝',
+  wechat: '微信',
+  stripe: 'Stripe',
+  apple: 'Apple In-App Purchase',
+  b2b: 'B2B许可授权'
+};
+
+export const orderKinds: Record<OrderKind, string> = {
+  create: '新建会员',
+  renew: '续订',
+  upgrade: '升级'
+}
