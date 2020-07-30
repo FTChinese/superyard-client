77c2dc6ab43a246d44d1717fc06cd81295866afc
weiguo.ni <neefrankie@gmail.com>
2020-07-30 10:29:47 +0800

    feat: add modal service and modal background

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ModalService } from './shared/service/modal.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  constructor() { }
+export class AppComponent implements OnInit{
+  constructor(
+    readonly modal: ModalService,
+  ) { }
+
+  ngOnInit() {
+
+  }
 }
