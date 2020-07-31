f37f406e6ce5014cf82293a21c807ccf8af3c519
weiguo.ni <neefrankie@gmail.com>
2020-07-31 15:57:25 +0800

    refactor: AccessToken and OAuthApp

--- src/app/data/schema/oauth.ts
+++ src/app/data/schema/oauth.ts
@@ -1,17 +1,12 @@
 import { ApiKeyKind } from './enum';
 
-// IAppBase contains user submitted data only.
-// Backedn will add extra meta fields.
-export interface AppBase {
+export interface OAuthApp {
   name: string;
   slug: string;
   repoUrl: string;
   description: string | null;
   homeUrl: string | null;
   callbackUrl: string | null;
-}
-
-export interface OAuthApp extends AppBase {
   clientId: string;
   clientSecret: string; // client id and secret are generated on server
   isActive: boolean; // Any new app is active by default unless it is deleted alter.
@@ -20,20 +15,16 @@ export interface OAuthApp extends AppBase {
   ownedBy: string; // The current loggedin user's name.
 }
 
-// Form data submitted to create access token for an app.
-export interface TokenBase {
-  description: string | null;
-  createdBy: string;
-  clientId: string | null;
-}
-
 // Access token / Personal access key returned from server.
-export interface AccessToken extends TokenBase {
+export interface AccessToken {
   id: number;
   token: string;
-  kind: ApiKeyKind;
   isActive: boolean;
   expiresIn: number | null;
+  kind: ApiKeyKind;
+  description: string | null;
+  clientId: string | null;
+  createdBy: string;
   createdAt: string;
   updatedAt: string;
   lastUsedAt: string | null;
