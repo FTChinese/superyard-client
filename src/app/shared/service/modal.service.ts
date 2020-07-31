cc68c239c7b79b9516804b40297e79b34aa945c9
weiguo.ni <neefrankie@gmail.com>
2020-07-31 16:01:52 +0800

    feat: add an id to modal service

--- src/app/shared/service/modal.service.ts
+++ src/app/shared/service/modal.service.ts
@@ -7,15 +7,22 @@ export class ModalService {
 
   private clsName = 'modal-open';
   on = false;
+  // id is used to distinguish which dialog should be
+  // shown when there are multiple modal dialogs on
+  // on the same page.
+  // Use both on and id to determine which one should be visible.
+  id: string;
 
   constructor() { }
 
-  open() {
+  open(id?: string) {
+    this.id = id;
     document.body.classList.add(this.clsName);
     this.on = true;
   }
 
   close() {
+    this.id = undefined;
     document.body.classList.remove(this.clsName);
     this.on = false;
   }
