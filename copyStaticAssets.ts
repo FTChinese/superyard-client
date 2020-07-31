691c49428039823075834809b17cb478bb1d980a
weiguo.ni <neefrankie@gmail.com>
2020-07-31 16:15:11 +0800

    fix: path

--- copyStaticAssets.ts
+++ copyStaticAssets.ts
@@ -5,12 +5,15 @@ import config from './bin/config';
 shell.mkdir('-p', config.htmlCopyTarget);
 shell.mkdir('-p', config.jsCssCopyTarget);
 
+console.log('Copying JS file to %s', config.jsCssCopyTarget);
 shell.cp('dist/superyard-client/*.js', config.jsCssCopyTarget);
 
 if (config.production) {
+  console.log('Copying CSS file to %s', config.jsCssCopyTarget)
   shell.cp('dist/superyard-client/*.css', config.jsCssCopyTarget);
 } else {
   shell.cp('dist/superyard-client/*.map', config.jsCssCopyTarget);
 }
 
-shell.cp(config.goViewFile, config.htmlCopyTarget);
+console.log('Copying HTML template to %s', config.htmlCopyTarget);
+shell.cp(config.goOutFile, config.htmlCopyTarget);
