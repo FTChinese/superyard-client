99d629a64a2baf02bc75017cb2ee999e02068f21
weiguo.ni <neefrankie@gmail.com>
2020-08-04 16:45:09 +0800

    feat: new product could add pricing plans

--- src/app/modules/product/page/new-product/new-product.component.ts
+++ src/app/modules/product/page/new-product/new-product.component.ts
@@ -1,28 +1,64 @@
 import { Component, OnInit } from '@angular/core';
-import { BaseProduct } from 'src/app/data/schema/product';
-import { ProductBuilderService } from 'src/app/core/service/product-builder.service';
+import { FormGroup, FormArray } from '@angular/forms';
+import { Tier } from 'src/app/data/schema/enum';
+import { DynamicControlService } from 'src/app/shared/service/dynamic-control.service';
+import { buildProductControls, buildPlanControls } from '../../control-builder';
+import { CreateProductForm } from 'src/app/data/schema/form-data';
 
 @Component({
   selector: 'app-new-product',
   templateUrl: './new-product.component.html',
-  styleUrls: ['./new-product.component.scss']
+  styleUrls: ['./new-product.component.scss'],
 })
 export class NewProductComponent implements OnInit {
 
-  product: BaseProduct;
+  productControls = buildProductControls();
+
+  planControls = buildPlanControls();
+
+  form: FormGroup;
 
   constructor(
-    private builderService: ProductBuilderService
+    readonly controlService: DynamicControlService
   ) { }
 
   ngOnInit(): void {
+    this.form = this.controlService.toFormGroup(this.productControls);
+
+    this.form.addControl('plans', new FormArray([]));
+
+    // Monitor the product tier selected.
+    // If it is premium, prices could not set billing cycle to month and forced to be year.
+    this.form.get('tier').valueChanges.subscribe((v: Tier) => {
+      this.planControls = buildPlanControls(v);
+
+      // If user changed product's tier to premium after FormArray already exists,
+      // force all form groups in the FormArray to set
+      // cycle value to year.
+      this.plansGroups.controls.forEach(fg => {
+        fg.patchValue({
+          cycle: 'year'
+        });
+      });
+
+    });
   }
 
-  // When host ProductFormComponent emitted `created` event,
-  // update ui to preview it and aslo updte the builder serive.
-  onCreated(p: BaseProduct) {
-    console.log('A new plan is created: %o', p);
-    this.product = p;
-    this.builderService.setProduct(p);
+  get plansGroups() {
+    return this.form.get('plans') as FormArray;
+  }
+
+  addPlan() {
+    this.plansGroups.push(this.controlService.toFormGroup(this.planControls));
+  }
+
+  onRemovePrice(i: number) {
+    this.plansGroups.removeAt(i);
+  }
+
+  onSubmit() {
+    const formData: CreateProductForm = this.form.value;
+
+    console.log(formData);
   }
 }
