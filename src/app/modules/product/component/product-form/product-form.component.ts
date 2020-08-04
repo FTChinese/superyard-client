fa35b46a7b921bcc05a795f741d9a36b604abde5
weiguo.ni <neefrankie@gmail.com>
2020-08-04 16:47:43 +0800

    refactor for deprecation

--- src/app/modules/product/component/product-form/product-form.component.ts
+++ src/app/modules/product/component/product-form/product-form.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { FormService } from 'src/app/shared/service/form.service';
 import { DynamicControl, DropdownControl, InputControl, TextareaControl } from 'src/app/shared/widget/control';
 import { Button } from 'src/app/shared/widget/button';
-import { BaseProduct } from 'src/app/data/schema/product';
-import { switchMap } from 'rxjs/operators';
+import { BaseProduct, Product } from 'src/app/data/schema/product';
 import { ProductForm } from 'src/app/data/schema/form-data';
-import { of } from 'rxjs';
 import { toISODatetimeUtc } from 'src/app/data/formatter/datetime';
-import { RequestError } from 'src/app/data/schema/request-result';
 import { Validators } from '@angular/forms';
-import { Tier } from 'src/app/data/schema/enum';
+import { tierOpts } from 'src/app/data/schema/enum';
 
 @Component({
   selector: 'app-product-form',
@@ -19,95 +16,87 @@ import { Tier } from 'src/app/data/schema/enum';
 })
 export class ProductFormComponent implements OnInit {
 
-  @Input() product: BaseProduct;
-  @Output() created = new EventEmitter<BaseProduct>();
+  @Input() product: Product;
 
-  controls: DynamicControl[]
-  button: Button = Button.primary().setName('Save');;
+  controls: DynamicControl[] = [
+    new DropdownControl({
+      value: '',
+      key: 'tier',
+      validators: [Validators.required],
+      label: 'Tier',
+      options: tierOpts
+    }),
+    new InputControl({
+      value: '',
+      key: 'heading',
+      validators: [Validators.required],
+      label: 'Heading',
+      type: 'text',
+      desc: 'Required',
+    }),
+    new TextareaControl({
+      value: '',
+      key: 'description',
+      validators: [Validators.required],
+      label: 'Description',
+      desc: 'Required. {{}} and content inside it are placeholders. Do not touch them unless you really mean to remove them.'
+    }),
+    new InputControl({
+      value: null,
+      key: 'smallPrint',
+      label: 'Small print',
+      type: 'text',
+      desc: 'Optional. Legal notice.'
+    }),
+  ];
+  button: Button = Button.primary().setName('Save');
 
   constructor(
     private formService: FormService,
   ) { }
 
   ngOnInit(): void {
-    this.controls = [
-      new DropdownControl({
-        value: this.product?.tier || '',
-        key: 'tier',
-        validators: [Validators.required],
-        label: 'Tier',
-        options: [
-          {
-            disabled: !(this.enableTier('standard')),
-            name: 'Standard',
-            value: 'standard'
-          },
-          {
-            disabled: !(this.enableTier('premium')),
-            name: 'Premium',
-            value: 'premium',
-          }
-        ]
-      }),
-      new InputControl({
-        value: this.product?.heading || '',
-        key: 'heading',
-        validators: [Validators.required],
-        label: 'Heading',
-        type: 'text',
-        desc: 'Required',
-      }),
-      new TextareaControl({
-        value: this.product?.description.join('\n') || '',
-        key: 'description',
-        validators: [Validators.required],
-        label: 'Description',
-        desc: 'Required. {{}} and content inside it are placeholders. Do not touch them unless you really mean to remove them.'
-      }),
-      new InputControl({
-        value: this.product?.smallPrint || null,
-        key: 'smallPrint',
-        label: 'Small print',
-        type: 'text',
-        desc: 'Optional. Legal notice.'
-      }),
-    ];
 
-    this.formService.formSubmitted$.pipe(
-      switchMap(data => {
-        console.log(data);
-        const formData: ProductForm = JSON.parse(data);
+    this.formService.formCreated$.subscribe(form => {
+      if (this.product) {
+        form.patchValue({
+          tier: this.product.tier,
+          heading: this.product.heading,
+          description: this.product.description.join('\n'),
+          smallPrint: this.product.smallPrint,
+        });
+        form.get('tier').disable();
+      }
+    });
 
-        const prod: BaseProduct = {
-          id: 'prod_duplicated',
-          tier: formData.tier,
-          heading: formData.heading,
-          description: formData.description.split('\n'),
-          smallPrint: formData.smallPrint,
-          createdUtc: toISODatetimeUtc(new Date()),
-          createdBy: 'weiguo.ni'
-        };
+    this.formService.formSubmitted$.subscribe(data => {
+      console.log(data);
+      const formData: ProductForm = JSON.parse(data);
 
-        return of(prod);
-      })
-    )
-    .subscribe({
-      next: product => {
-        console.log(product);
-        this.created.emit(product);
-      },
-      error: errResp => {
-        console.log(errResp);
-        this.formService.sendError(RequestError.fromResponse(errResp));
+      const prod: BaseProduct = {
+        id: 'prod_duplicated',
+        tier: formData.tier,
+        heading: formData.heading,
+        description: formData.description.split('\n'),
+        smallPrint: formData.smallPrint,
+        createdUtc: toISODatetimeUtc(new Date()),
+        updatedUtc: toISODatetimeUtc(new Date()),
+        createdBy: 'weiguo.ni'
+      };
+
+      if (this.product) {
+        this.update(prod);
+      } else {
+        this.create(prod);
       }
     });
   }
 
-  private enableTier(t: Tier): boolean {
-    if (!this.product) {
-      return true;
-    }
+  private create(p: BaseProduct) {
+    console.log(p);
+  }
 
-    return this.product.tier === t;
+  private update(p: BaseProduct) {
+    console.log(p);
   }
 }
