894939f28612b9a78eb3f853263a5e26c11d8ff3
weiguo.ni <neefrankie@gmail.com>
2020-08-05 23:46:09 +0800

    feat: build discount controls

--- src/app/modules/product/control-builder.ts
+++ src/app/modules/product/control-builder.ts
@@ -39,20 +39,20 @@ export function buildProductControls(): DynamicControl[] {
 export function buildPlanControls(productTier?: Tier): DynamicControl[] {
   return [
     new InputControl({
-      value: '',
+      value: 0,
       key: 'price',
       validators: [
         Validators.required,
         Validators.min(0)
       ],
-      label: 'Price',
+      label: 'Price (Required)',
       type: 'number',
     }),
     new DropdownControl({
       value: productTier === 'premium' ? 'year' : '',
       key: 'cycle',
       validators: [Validators.required],
-      label: 'Billing Cycle',
+      label: 'Billing Cycle (Required)',
       options: cycleOpts.map(opt => {
         return {
           // Diable month for premium.
@@ -62,5 +62,71 @@ export function buildPlanControls(productTier?: Tier): DynamicControl[] {
         };
       }),
     }),
+    new InputControl({
+      value: '',
+      key: 'description',
+      validators: [
+        Validators.maxLength(32)
+      ],
+      label: 'Description (optional)',
+      type: 'text',
+    }),
   ];
+}
+
+export interface DiscountControls {
+  priceOff: DynamicControl;
+  startTimeRow: DynamicControl[];
+  endTimeRow: DynamicControl[];
+}
+
+export function buildDiscountControls(price: number): DiscountControls {
+  return {
+    priceOff: new InputControl({
+      value: 0,
+      key: 'priceOff',
+      validators: [
+        Validators.required,
+        Validators.min(0),
+        Validators.max(price),
+      ],
+      label: 'Price Off (Required)',
+      type: 'number',
+    }),
+    startTimeRow: [
+      // Value format: 2020-08-06T00:00
+      new InputControl({
+        value: '',
+        key: 'startDate',
+        validators: [
+          Validators.required,
+        ],
+        label: 'Start Date (Required)',
+        type: 'date',
+      }),
+      new InputControl({
+        value: '00:00:00',
+        key: 'startTime',
+        label: 'Start Time',
+        type: 'time',
+      }),
+    ],
+    endTimeRow: [
+      new InputControl({
+        value: '',
+        key: 'endDate',
+        validators: [
+          Validators.required,
+        ],
+        label: 'End Date (Required)',
+        type: 'date',
+      }),
+      new InputControl({
+        value: '00:00:00',
+        key: 'endTime',
+        label: 'End Time',
+        type: 'time',
+      })
+    ]
+  };
 }
