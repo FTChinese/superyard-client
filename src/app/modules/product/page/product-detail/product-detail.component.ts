75a8abd4b756c50d3301da07968cfbbcd17784df
weiguo.ni <neefrankie@gmail.com>
2020-08-05 23:49:21 +0800

    refactor: modal

--- src/app/modules/product/page/product-detail/product-detail.component.ts
+++ src/app/modules/product/page/product-detail/product-detail.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { Product, Plan } from 'src/app/data/schema/product';
+import { Product, Plan, Discount } from 'src/app/data/schema/product';
 import { ActivatedRoute } from '@angular/router';
 import { switchMap } from 'rxjs/operators';
 import { of } from 'rxjs';
 import { products } from 'src/app/data/schema/mocker';
 import { ModalService } from 'src/app/shared/service/modal.service';
 import { MenuItem, SelectedItem } from 'src/app/shared/widget/menu';
+import { ToastService } from 'src/app/shared/service/toast.service';
 
 @Component({
   selector: 'app-product-detail',
@@ -13,12 +14,17 @@ import { MenuItem, SelectedItem } from 'src/app/shared/widget/menu';
   styleUrls: ['./product-detail.component.scss']
 })
 export class ProductDetailComponent implements OnInit {
+  // Used to identify which modal to open and close.
+  private modalNewDiscount = 'd';
+  private modalRemoveDiscount = 'r';
+  private modalNewPrice = 'p';
 
   product: Product;
+  discountTarget: Plan;
 
   menuItems: MenuItem[] = [
     {
-      id: 'modify',
+      id: this.modalNewDiscount,
       name: 'Modify discount'
     },
     {
@@ -26,19 +32,32 @@ export class ProductDetailComponent implements OnInit {
       divider: true,
     },
     {
-      id: 'remove',
+      id: this.modalRemoveDiscount,
       name: 'Remove discount',
       danger: true,
     }
   ];
 
-  get modalTitle(): string {
+  get priceModalTitle(): string {
     return `New pricing for ${this.product.tier} product`;
+  }
+
+  get priceModalOn(): boolean {
+    return this.modal.on && this.modal.id === this.modalNewPrice;
+  }
+
+  get newDiscountModalOn(): boolean {
+    return this.modal.on && this.modal.id === this.modalNewDiscount;
+  }
+
+  get removeDiscountModalOn(): boolean {
+    return this.modal.on && this.modal.id === this.modalRemoveDiscount;
   }
 
   constructor(
     private route: ActivatedRoute,
-    readonly modal: ModalService
+    readonly modal: ModalService,
+    private toast: ToastService,
   ) { }
 
   ngOnInit(): void {
@@ -54,20 +73,61 @@ export class ProductDetailComponent implements OnInit {
     });
   }
 
-  showNewPricing() {
-    this.modal.open();
+  // Use sectionIndex to find the plan, cellIndex to identify which menu is cliekd.
+  onSelectMenu(position: SelectedItem) {
+    console.log('Selected %o', position);
+
+    const plan = this.product.plans[position.sectionIndex];
+
+    if (!plan) {
+      this.toast.error(`Plan not found at position ${position.sectionIndex}`);
+      return;
+    }
+
+    this.discountTarget = plan;
+
+    const menuItem = this.menuItems[position.cellIndex];
+
+    switch (menuItem.id) {
+      case this.modalNewDiscount:
+        this.modal.open(this.modalNewDiscount);
+        break;
+
+      case this.modalRemoveDiscount:
+        this.modal.open(this.modalRemoveDiscount);
+        break;
+
+      default:
+        this.toast.error(`Menu command at postion ${position.cellIndex} not found`);
+    }
   }
 
-  onPriceCreated(ok: boolean) {
+  onDiscountCreated(discount: Discount) {
     this.modal.close();
+    console.log(discount);
+    this.discountTarget = undefined;
+  }
+
+  onDiscountRemoved(ok: boolean) {
+    this.modal.close();
+    this.discountTarget = undefined;
+  }
+
+  // Open new price form dialog.
+  onNewPrice() {
+    this.modal.open(this.modalNewPrice);
+  }
+
+  // Close price form after created.
+  onPriceCreated(plan: Plan) {
+    console.log(plan);
+    this.modal.close();
+    this.toast.info('New price is created. Refreshing...');
     // TODO: refresh prices list.
   }
 
+  // Put a plan under a product's default list so that it is visible on paywall.
+  // Request data: product_id: string, plan_id: string.
   onDefaultPlan(plan: Plan) {
-
-  }
-
-  onSelectMenu(position: SelectedItem) {
-    console.log('Selected %o', position);
   }
 }
