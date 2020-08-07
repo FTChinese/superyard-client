5af578ea0b6556a0ede8690bc4c50d88618937cb
weiguo.ni <neefrankie@gmail.com>
2020-08-07 12:59:30 +0800

    refactor:  spinner is determined by button progress state

--- src/app/shared/progress-btn/progress-btn.component.ts
+++ src/app/shared/progress-btn/progress-btn.component.ts
@@ -9,20 +9,14 @@ import { Button } from '../widget/button';
 export class ProgressBtnComponent implements OnInit {
 
   @Input() button: Button;
+  // Allow explicitly disabled the button. A disabled button does not
+  // indicates spinner should show up. If disabled is false, we will
+  // futher look whether button is in proress state. If it is, the button
+  // is disabled since you should always disable it when in progress.
+  // If you do not set the disabled value, things becomes simpler as the
+  // button's disabled state is solely determined by the button.inProgress.
+  // This variable exists mostly to handle form.valid state.
   @Input() disabled = false;
-
-  // Determine whether the spinner should be visible.
-  // It is pertially affected the the disabled state of button:
-  // if button is not disabled, spinner should never be shown;
-  // if button is disabled, the visibility of spinner depends on
-  // inProgress state.
-  get loading(): boolean {
-    if (!this.disabled) {
-      return false;
-    }
-
-    return this.button.inProgress;
-  }
 
   constructor() { }
 
