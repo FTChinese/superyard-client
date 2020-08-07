ef813c1d08a0585d7548105bdadd0266cce870b7
weiguo.ni <neefrankie@gmail.com>
2020-08-07 12:58:45 +0800

    feat: define a alert builder

--- src/app/shared/widget/alert.ts
+++ src/app/shared/widget/alert.ts
@@ -1,7 +1,56 @@
 export type AlertType = 'success' | 'info' | 'warning' | 'danger' | 'primary' | 'secondary' | 'light' | 'dark';
 
-export interface Alert {
-  type: AlertType;
+export class Alert {
+  type: AlertType = 'success';
   message: string;
-  dismissible: boolean;
+  dismissible = true;
+
+  constructor(m: string) {
+    this.message = m;
+  }
+
+  static info(m: string): Alert {
+    return (new Alert(m))
+      .setType('info');
+  }
+
+  static warning(m: string): Alert {
+    return (new Alert(m))
+      .setType('warning');
+  }
+
+  static danger(m: string): Alert {
+    return (new Alert(m))
+      .setType('danger');
+  }
+
+  static primary(m: string): Alert {
+    return (new Alert(m))
+      .setType('primary');
+  }
+
+  static secondary(m: string): Alert {
+    return (new Alert(m))
+      .setType('secondary');
+  }
+
+  static light(m: string): Alert {
+    return (new Alert(m))
+      .setType('light');
+  }
+
+  static dark(m: string): Alert {
+    return (new Alert(m))
+      .setType('dark');
+  }
+
+  setDismissible(d: boolean): Alert {
+    this.dismissible = d;
+    return this;
+  }
+
+  setType(t: AlertType): Alert {
+    this.type = t;
+    return this;
+  }
 }
