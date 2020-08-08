074e78b549386f58dce16c1f2d2e16d2b42db49f
weiguo.ni <neefrankie@gmail.com>
2020-08-08 22:50:24 +0800

    feat: banner box component to show banner content

---
+++ src/app/modules/product/component/banner-box/banner-box.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Banner, BaseBanner } from 'src/app/data/schema/paywall';
+
+@Component({
+  selector: 'app-banner-box',
+  templateUrl: './banner-box.component.html',
+  styleUrls: ['./banner-box.component.scss']
+})
+export class BannerBoxComponent implements OnInit {
+  @Input() banner: BaseBanner;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
