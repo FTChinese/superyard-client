0b2b2f0d1a7cc4e9519ec919f32c1cf7cbe2cd5a
weiguo.ni <neefrankie@gmail.com>
2020-08-08 22:54:43 +0800

    feat: define Period

---
+++ src/app/data/schema/period.ts
@@ -0,0 +1,30 @@
+import { DateTime, concateISODateTime } from '../formatter/datetime';
+
+// Period defines a range of time.
+export interface Period {
+  startUtc: string;
+  endUtc: string;
+}
+
+// FormPeriod defines the structure of datetie entered in a form.
+// The input control has date and time type separately, therefore
+// we ask user to enter date and time in separate input and concatenate
+// theme together with browser's timezone.
+export interface FormPeriod {
+  startUtc: DateTime;
+  endUtc: DateTime;
+}
+
+// Turn data submitted by a form to Period type.
+export function buildPeriod(p: FormPeriod & { zone: string }): Period {
+  return {
+    startUtc: concateISODateTime({
+      ...p.startUtc,
+      zone: p.zone
+    }),
+    endUtc: concateISODateTime({
+      ...p.endUtc,
+      zone: p.zone
+    })
+  };
+}
