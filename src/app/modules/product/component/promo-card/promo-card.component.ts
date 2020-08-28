bf84bb006a076ee91bb987c152aed7d257be8501
weiguo.ni <neefrankie@gmail.com>
2020-08-28 17:29:33 +0800

    feat: use api data

--- src/app/modules/product/component/promo-card/promo-card.component.ts
+++ src/app/modules/product/component/promo-card/promo-card.component.ts
@@ -3,6 +3,7 @@ import { LoadingResult } from 'src/app/shared/widget/progress';
 import { Promo } from 'src/app/data/schema/paywall';
 import { MetaItem } from 'src/app/shared/widget/meta-data';
 import { Link } from 'src/app/shared/widget/link';
+import { isEmpty } from 'rxjs/operators';
 
 /**
  * This component is used to show the promotion details on the paywall page.
@@ -13,21 +14,35 @@ import { Link } from 'src/app/shared/widget/link';
   styleUrls: ['./promo-card.component.scss']
 })
 export class PromoCardComponent implements OnInit {
-  @Input() result: LoadingResult<Promo>;
+  // It uses the shared BaseBanner fieds to reuse BannerBoxComponent.
+  @Input() promo: Promo;
+
+  get isEmpty(): boolean {
+    return !this.promo || !this.promo.id;
+  }
+
+  get termsAndConditions(): string[] {
+    if (isEmpty) {
+      return [];
+    }
+
+    return this.promo.terms.split('\n')
+      .filter(line => line !== '');
+  }
 
   get metaItems(): MetaItem[] {
-    if (!this.result || !this.result.value) {
+    if (!this.promo) {
       return [];
     }
 
     return [
       {
         label: 'Created by',
-        value: this.result.value.createdBy
+        value: this.promo.createdBy
       },
       {
         label: 'Create at',
-        value: this.result.value.createdUtc
+        value: this.promo.createdUtc
       },
     ];
   }
