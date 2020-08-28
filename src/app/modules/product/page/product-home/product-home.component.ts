d4a5cc666c4e8b2562995e94133019d516f78a5d
weiguo.ni <neefrankie@gmail.com>
2020-08-28 17:32:28 +0800

    feat: add icon to indicate whether a product is on paywall

--- src/app/modules/product/page/product-home/product-home.component.ts
+++ src/app/modules/product/page/product-home/product-home.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { PricedProduct } from 'src/app/data/schema/product';
-import { productStd, productPrm } from 'src/app/data/schema/mocker';
+import { ProductService } from '../../service/product.service';
+import { ProgressService } from 'src/app/shared/service/progress.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { RequestError } from 'src/app/data/schema/request-result';
+import { ToastService } from 'src/app/shared/service/toast.service';
 
 @Component({
   selector: 'app-product-home',
@@ -9,14 +13,55 @@ import { productStd, productPrm } from 'src/app/data/schema/mocker';
 })
 export class ProductHomeComponent implements OnInit {
 
-  products: PricedProduct[] = [
-    productStd,
-    productPrm,
-  ];
+  products: PricedProduct[];
 
   constructor(
-  ) { }
+    private productService: ProductService,
+    readonly progress: ProgressService,
+    private toast: ToastService
+  ) {
+    this.progress.start();
+  }
 
   ngOnInit(): void {
+    this.productService.listProducts()
+      .subscribe({
+        next: products => {
+          this.progress.stop();
+          this.products = products;
+        },
+         error: (err: HttpErrorResponse) => {
+          const errRes = new RequestError(err);
+
+          this.toast.error(errRes.message);
+         }
+      });
+  }
+
+  useProduct(prodId: string) {
+    this.progress.start();
+
+    this.productService.activateProduct(prodId)
+      .subscribe({
+        next: product => {
+          this.progress.stop();
+          this.products.forEach(prod => {
+            if (prod.tier !== product.tier) {
+              return;
+            }
+
+            if (prod.id !== product.id) {
+              prod.isActive = false;
+            } else {
+              prod.isActive = true;
+            }
+          });
+        },
+        error: (err: HttpErrorResponse) => {
+          this.progress.stop();
+          const reqErr = new RequestError(err);
+          this.toast.error(reqErr.message);
+        }
+      })
   }
 }
