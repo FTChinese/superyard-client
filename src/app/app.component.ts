e965503990ac1b9acf1aede7186ddd48de4d3760
weiguo.ni <neefrankie@gmail.com>
2020-08-28 17:20:04 +0800

    feat: add global spinner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalService } from './shared/service/modal.service';
+import { ProgressService } from './shared/service/progress.service';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,7 @@ import { ModalService } from './shared/service/modal.service';
 export class AppComponent implements OnInit{
   constructor(
     readonly modal: ModalService,
+    readonly progress: ProgressService,
   ) { }
 
   ngOnInit() {
