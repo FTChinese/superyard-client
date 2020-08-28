c8449adae9c652a67c7d0cc525eebc0616aa5307
weiguo.ni <neefrankie@gmail.com>
2020-08-28 17:29:06 +0800

    feat: connect to api

--- src/app/modules/product/component/price-form/price-form.component.ts
+++ src/app/modules/product/component/price-form/price-form.component.ts
@@ -1,9 +1,14 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { PlanForm, buildPlanControls, PlanReq } from "../../schema/PlanForm";
+import { PlanForm, buildPlanControls, PlanReq, planFormToReq } from '../../schema/PlanForm';
 import { Button } from 'src/app/shared/widget/button';
 import { FormService } from 'src/app/shared/service/form.service';
-import { PricedProduct, Plan } from 'src/app/data/schema/product';
+import { ExpandedPlan, Product, Plan } from 'src/app/data/schema/product';
 import { ToastService } from 'src/app/shared/service/toast.service';
+import { ProgressService } from 'src/app/shared/service/progress.service';
+import { ProductService } from '../../service/product.service';
+import { DynamicControl } from 'src/app/shared/widget/control';
+import { HttpErrorResponse } from '@angular/common/http';
+import { RequestError } from 'src/app/data/schema/request-result';
 
 @Component({
   selector: 'app-price-form',
@@ -14,21 +19,21 @@ import { ToastService } from 'src/app/shared/service/toast.service';
 export class PriceFormComponent implements OnInit {
 
   // For which product this price is made.
-  @Input() product: PricedProduct;
+  @Input() product: Product;
   @Output() created = new EventEmitter<Plan>();
 
-  controls = buildPlanControls();
+  controls: DynamicControl[];
   button = Button.primary().setName('Create');
 
   constructor(
     private formService: FormService,
-    private toast: ToastService
+    private toast: ToastService,
+    readonly progress: ProgressService,
+    private productService: ProductService
   ) { }
 
   ngOnInit(): void {
-    if (this.product.tier === 'premium') {
-      this.controls = buildPlanControls(this.product.tier);
-    }
+    this.controls = buildPlanControls(this.product.tier);
 
     this.formService.formSubmitted$.subscribe(data => {
       const formData: PlanForm = JSON.parse(data);
@@ -37,18 +42,26 @@ export class PriceFormComponent implements OnInit {
     });
   }
 
+  /**
+   * @description Create a new pricing plan.
+   * Pay attention to the price which should be a number but the form submitted is a string.
+   */
   private create(formData: PlanForm) {
-    const reqData: PlanReq = {
-      productId: this.product.id,
-      price: formData.price,
-      tier: this.product.tier,
-      cycle: formData.cycle,
-      description: formData.description,
-    };
+    const reqData: PlanReq = planFormToReq(this.product, formData);
 
     console.log('Plan request: %o', reqData);
 
-    this.created.emit(genPlan(reqData));
-    this.toast.info('New pricing plan created!');
+    this.productService.createPlan(reqData)
+      .subscribe({
+        next: p => {
+          this.toast.info('New pricing plan created!');
+          this.created.emit(p);
+        },
+        error: (err: HttpErrorResponse) => {
+          const reqErr = new RequestError(err);
+          this.formService.sendError(reqErr);
+        }
+      });
+
   }
 }
