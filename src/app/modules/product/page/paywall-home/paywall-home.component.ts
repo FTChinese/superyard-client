92ecff501a21f78429dafe06a5b99655353a7161
weiguo.ni <neefrankie@gmail.com>
2020-09-01 10:58:44 +0800

    refactor: refreshing paywall

--- src/app/modules/product/page/paywall-home/paywall-home.component.ts
+++ src/app/modules/product/page/paywall-home/paywall-home.component.ts
@@ -6,6 +6,7 @@ import { HttpErrorResponse } from '@angular/common/http';
 import { RequestError } from 'src/app/data/schema/request-result';
 import { ProgressService } from 'src/app/shared/service/progress.service';
 import { zeroPromo } from '../../schema/PromoForm';
+import { ModalService } from 'src/app/shared/service/modal.service';
 
 @Component({
   selector: 'app-paywall-home',
@@ -19,10 +20,12 @@ export class PaywallHomeComponent implements OnInit {
   loadingError: string;
   notFound: boolean;
 
+  pwJson: string;
+
   constructor(
     private paywallService: PaywallService,
     private toast: ToastService,
-    readonly progress: ProgressService
+    readonly progress: ProgressService,
   ) {
     this.progress.start();
   }
@@ -57,9 +60,26 @@ export class PaywallHomeComponent implements OnInit {
   rebuild() {
     this.toast.info('Refreshing Subscription API Cache...');
     this.progress.start();
+
+    this.paywallService.rebuild().subscribe({
+      next: data => {
+        this.progress.stop();
+        this.pwJson = data;
+      },
+      error: (err: HttpErrorResponse) => {
+        this.progress.stop();
+
+        const reqErr = new RequestError(err);
+        this.toast.error(reqErr.message);
+      }
+    });
   }
 
   onPromoDropped() {
     this.paywall.promo = zeroPromo();
+  }
+
+  onClose() {
+    this.pwJson = undefined;
   }
 }
