9f7a40ba25b5f08da7448f13a0ea099aa66f3295
neefrankie <neefrankie@gmail.com>
2020-09-12 21:04:39 +0800

    refactor: admin router

--- src/app/modules/admin/admin-routing.module.ts
+++ src/app/modules/admin/admin-routing.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-import { AdminHomeComponent } from './page/admin-home/admin-home.component';
-import { StaffHomeComponent } from './page/staff-home/staff-home.component';
 import { UpdateUserComponent } from './page/update-user/update-user.component';
 import { CreateUserComponent } from './page/create-user/create-user.component';
-import { SearchResultsComponent } from './components/search-results/search-results.component';
-import { StaffListComponent } from './components/staff-list/staff-list.component';
+import { StaffListComponent } from './page/staff-list/staff-list.component';
+import { VipListComponent } from './page/vip-list/vip-list.component';
+
 
 const routes: Routes = [
   {
@@ -16,31 +14,19 @@ const routes: Routes = [
   },
   {
     path: 'staff',
-    component: StaffHomeComponent,
-    children: [
-      {
-        path: '',
-        component: StaffListComponent,
-      },
-      {
-        path: 'search-results',
-        component: SearchResultsComponent,
-      },
-    ]
+    component: StaffListComponent,
   },
   {
-    path: 'staff',
-    component: AdminHomeComponent,
-    children: [
-      {
-        path: 'new',
-        component: CreateUserComponent,
-      },
-      {
-        path: ':id',
-        component: UpdateUserComponent,
-      }
-    ]
+    path: 'staff/new',
+    component: CreateUserComponent,
+  },
+  {
+    path: 'staff/:id',
+    component: UpdateUserComponent,
+  },
+  {
+    path: 'vip',
+    component: VipListComponent,
   }
 ];
 
